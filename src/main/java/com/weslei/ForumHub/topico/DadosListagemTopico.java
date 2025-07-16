3bc521b3fd992802d9f31befa90a38a84b41a3d2
Weslei Augusto C <85812820+weslei573@users.noreply.github.com>
2025-07-16 17:12:06 -0400

    lidando com erros

--- src/main/java/com/weslei/ForumHub/topico/DadosListagemTopico.java
+++ src/main/java/com/weslei/ForumHub/topico/DadosListagemTopico.java
@@ -2,15 +2,14 @@ package com.weslei.ForumHub.topico;
 
 import java.time.LocalDateTime;
 
-public record DadosListagemTopico(Long id, String titulo, String mensagem, LocalDateTime dataCriacao, StatusTopico status
+public record DadosListagemTopico(Long id, String titulo, String mensagem, LocalDateTime dataCriacao
 ) {
     public DadosListagemTopico(Topico topico){
         this(
                 topico.getId(),
                 topico.getTitulo(),
                 topico.getMensagem(),
-                topico.getDataCriacao(),
-                topico.getStatus()
+                topico.getDataCriacao()
         );
     }
 }
